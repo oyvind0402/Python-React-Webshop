92d149f2489222d197c1cc4b2520b06aa980e299
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2021-04-30 19:15:31 +0200

    Added the description to products, added an adminpage with a form that I will try to test the post method of a product on at some point. Also added the post and get method for a specific product, but I'm not sure if they work.

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE product (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(20) NOT NULL,
+    description TEXT NOT NULL,
     price INT NOT NULL,
     brand VARCHAR(20) NOT NULL,
     category VARCHAR(20) NOT NULL,
@@ -16,6 +17,6 @@ CREATE TABLE user (
 
 INSERT INTO user (username, password) VALUES ('admin', 'admin');
 
-INSERT INTO product (name, price, brand, category) VALUES ('iPhone 12', '12000', 'Apple', 'Phone');
-INSERT INTO product (name, price, brand, category) VALUES ('OnePlus 8 Pro', '8999', 'OnePlus', 'Phone');
-INSERT INTO product (name, price, brand, category) VALUES ('OMEN by HP 17,3"', '14000', 'HP', 'Laptop');
+INSERT INTO product (name, description, price, brand, category) VALUES ('iPhone 12', 'This is a kind of phone', '12000', 'Apple', 'Phone');
+INSERT INTO product (name, description, price, brand, category) VALUES ('OnePlus 8 Pro', 'This is a kind of phone', '8999', 'OnePlus', 'Phone');
+INSERT INTO product (name, description, price, brand, category) VALUES ('OMEN by HP 17,3"', 'This is a laptop', '14000', 'HP', 'Laptop');
--- shop/shop.py
+++ shop/shop.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
 import mysql.connector
 
 app = Flask(__name__, static_folder="/shop/static", static_url_path="")
@@ -30,6 +30,29 @@ def getProducts():
     cursor.close()
     db.close()
     return jsonify(result), 200
+
+@app.route('/api/product/<int:productid>', methods=["GET"])
+def getProduct(productid):
+    db = openDatabase()
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM product WHERE id=%s', [productid])
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return jsonify(result), 200
+
+@app.route('/api/product/add', methods=["POST"])
+def addProduct():
+    name = request.form['name']
+    price = request.form['price']
+    description = request.form['description']
+    brand = request.form['brand']
+    category = request.form['category']
+    cursor.execute('INSERT INTO product (name, description, price, brand, category) VALUES (%s, %s, %s, %s, %s)', (name, description, price, brand, category))
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return jsonify(result), 201
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
