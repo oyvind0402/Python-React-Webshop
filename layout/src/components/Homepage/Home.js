d32c3625c0ebbb412f38dff78f8aef298609e665
mbaan2 <mbaan2@gmail.com>
2021-05-11 20:59:58 +0200

    Auto load of products works

--- layout/src/components/Homepage/Home.js
+++ layout/src/components/Homepage/Home.js
@@ -8,40 +8,39 @@ import { ProductCard } from "./ProductCard";
 export default function Home(props) {
   const [products, updateProducts] = useState([]);
 
-  const aList = [
-    {brand: "Apple", color: "yellow"},
-    {brand: "Samsung", color: "green"},
-    {brand: "Nokia", color: "Blue"},
-  ]
-
   useEffect(() => {
-    updateProducts(aList);
+    const loadData = async () =>{
+      const response = await fetch("http://localhost:5000/api/products");
+      let data = await response.json();
+      updateProducts(data);
+    }
+    loadData()
   }, []);
 
-  function getProduct(filterOptions) {
+  async function getProduct(filterOptions) {
     //   I AM NOT GETTING THE LATEST VERSION OF FILTEROPTIONS
-    // const response = await fetch("http://localhost:5000/api/products");
-    // let data = await response.json();
-    let data = [
-      {brand: "Apple", color: "yellow"},
-      {brand: "Samsung", color: "green"},
-      {brand: "Nokia", color: "Blue"},
-    ];
+    const response = await fetch("http://localhost:5000/api/products");
+    let data = await response.json();
+    // let data = [
+    //   {brand: "Apple", color: "yellow"},
+    //   {brand: "Samsung", color: "green"},
+    //   {brand: "Nokia", color: "Blue"},
+    // ];
     let newProducts = [];
     filterOptions.forEach((filter) => { //Loops through all the filters
-      if(filter[1].length !== 0){ //If filter is not empty we will have a further look
+      if (filter[1].length !== 0) { //If filter is not empty we will have a further look
         filter[1].forEach((element) => { //Loops through all the elements of a attribute
           data.forEach((product) => { //Loops through all the products received from API
-            for(let attribute in product){ // Looks at all the attributes of a product
-              if(filter[0].toLowerCase() === attribute){ //if the match continue
-                if(element.toLowerCase() === product[attribute].toLowerCase()) { // if element in filter equals from api we want it to show
+            for (let attribute in product) { // Looks at all the attributes of a product
+              if (filter[0].toLowerCase() === attribute) { //if the match continue
+                if (element.toLowerCase() === product[attribute].toLowerCase()) { // if element in filter equals from api we want it to show
                   let alreadyInList = false;
                   newProducts.forEach((oldProduct) => { //Checks if product is already in list
-                    if(oldProduct === product){
+                    if (oldProduct === product) {
                       alreadyInList = true;
                     }
                   })
-                  if(!alreadyInList){
+                  if (!alreadyInList) {
                     newProducts.push(product);
                   }
                 }
