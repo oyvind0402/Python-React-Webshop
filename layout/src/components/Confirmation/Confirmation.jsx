3772f6348cfc8db5b175dc1c1de627a7c4e8eebc
anafvana <anafvana@gmail.com>
2021-05-22 12:43:26 +0200

    Added header to Confirmation. Changed UserPage layout.

--- layout/src/components/Confirmation/Confirmation.jsx
+++ layout/src/components/Confirmation/Confirmation.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Header } from "../Header/Header";
 
 export const Confirmation = (props) => {
   console.log(props.location.state);
   if (props.location.state !== undefined) {
-    return <p>There's data</p>;
+    return (
+      <>
+        <Header />
+        <p>There's data</p>
+      </>
+    );
   } else {
-    return <p>Undefined</p>;
+    return (
+      <>
+        <Header />
+        <p>Undefined</p>
+      </>
+    );
   }
 };
